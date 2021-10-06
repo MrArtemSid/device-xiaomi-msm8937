fa202e64994a971a53cadb11e76ddc2dd4091ef3
MrArtemSid <artem.sid.key@gmail.com>
2021-10-06 17:10:36 +0400

    msm8937: Import adreno firmware from linux-firmware
    repo: https://kernel.googlesource.com/pub/scm/linux/kernel/git/firmware/linux-firmware.git
    state: 547b202

---
+++ firmware/device.mk
@@ -0,0 +1,12 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/a300_pfp.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a300_pfp.fw \
+    $(LOCAL_PATH)/a300_pm4.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a300_pm4.fw \
+    $(LOCAL_PATH)/a530_pfp.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_pfp.fw \
+    $(LOCAL_PATH)/a530_pm4.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_pm4.fw \
+    $(LOCAL_PATH)/a530v3_gpmu.fw2:$(TARGET_COPY_OUT_VENDOR)/firmware/a530v3_gpmu.fw2 \
+    $(LOCAL_PATH)/a530_zap.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_zap.b00 \
+    $(LOCAL_PATH)/a530_zap.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_zap.b01 \
+    $(LOCAL_PATH)/a530_zap.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_zap.b02 \
+    $(LOCAL_PATH)/a530_zap.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/a530_zap.mdt
+
+    
